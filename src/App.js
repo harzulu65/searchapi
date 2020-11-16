67e6279acc6299feda18fec70795b654baee6118
Harold Zuluaga <haroldzuluaga@aol.com>
2020-11-15 18:56:47 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React from "react";
+import { useState } from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const [apiKey, setApiKey] = useState("AIzaSyBBXFTunPPEx_fdU0HPkIWbEJkcFQE1BJ0");
+
+  function handleChange(event) {
+    const book = event.target.value;
+    setBook(book);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${book}&key=${apiKey}&maxResults=40`)
+    .then(data => {
+      console.log(data.data.items);
+      setResult(data.data.items);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <h1>Book Search Application</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+            <input 
+              type='text' onChange={handleChange}
+              className="form-control mt-10" 
+              input="input-control" 
+              placeholder="Search for Books" 
+              autoComplete="off"/>
+        </div>
+        <button type="submit" className="btn btn-danger">Search</button>
+      </form>
+
+      {result.map(book => (
+        <a target="_blank" href={book.volumeInfo.previewLink}>
+        <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
         </a>
-      </header>
+      ))}
     </div>
   );
 }
