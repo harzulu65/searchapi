453a4c7d945a084e5a9521c04bb2566f90bb8530
Harold Zuluaga <haroldzuluaga@aol.com>
2020-11-18 16:13:33 -0800

    update home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 function Home() {
     return <div className="container">
-        <h1>Home page</h1>
-        <p>Aliquam ultrices sagittis orci a scelerisque purus semper eget. Ut tristique et egestas quis ipsum suspendisse ultrices gravida.</p>
+        <h1>Google Books Api Search</h1>
+        <p>This App will get data from the Google Books API </p>
+        <p>It will render the pictures of the books with the title selected by the user.</p>
+        <p>In the Search for Books Input on the SEARCH option of this menu click on SEARCH</p>
+        <p>After that if user wants to save any of the books just click on the picture and 
+        it will be saved on the Mongo Database</p>
+        
+        <p>Modify option will read the MongoDB Atlas database and will show the books saved</p>
     </div>
 }
 
